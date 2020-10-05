7916e81905704b74bae89466636e2029e1fa04ea
v.kalnik <v.kalnik@mail.ru>
2020-10-05 19:26:51 +0300

    Создаём класс Snake.

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -24,7 +24,8 @@ namespace snake
             rightLine.Drow();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
                         
             Console.ReadLine();
         }
