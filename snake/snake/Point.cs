3e0937f460527cdbb83c91cc6ceb9586645e4fa6
v.kalnik <v.kalnik@mail.ru>
2020-10-06 19:30:53 +0300

    Змейка движется

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -55,6 +55,12 @@ namespace snake
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
         public override string ToString()
         {
             return x + ", " + y + ", " + sym;
